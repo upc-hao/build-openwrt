04125677dd55160aceea21f6ba9365dfd7afbe42
upc-hao <cao_hao@126.com>
2025-03-13 14:08:27 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -9,6 +9,6 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# other
+# others
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
